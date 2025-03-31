44cf3c601d4bf3b478d3db24f336274625f3eb78
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-03-31 22:18:45 +0500

    Init the Main activity

--- app/src/main/java/com/example/myapplication7/MainActivity.kt
+++ app/src/main/java/com/example/myapplication7/MainActivity.kt
@@ -1,48 +1,78 @@
-
 package com.example.myapplication7
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication7.ui.theme.MyApplication7Theme
+import android.os.Environment
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var tvData: TextView
+
+    companion object {
+        const val REQUEST_CODE_WRITE_PERM = 401
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MyApplication7Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        tvData = findViewById(R.id.tvData)
+
+        val btnWriteFile: Button = findViewById(R.id.btnWriteFile)
+        val btnReadFile: Button = findViewById(R.id.btnReadFile)
+
+        btnWriteFile.setOnClickListener {
+            writeFile("Hello: ${System.currentTimeMillis()}")
+        }
+
+        btnReadFile.setOnClickListener {
+            tvData.text = readFile()
+        }
+
+        requestNeededPermission()
+    }
+
+    private fun requestNeededPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE_WRITE_PERM)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_WRITE_PERM) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission NOT granted", Toast.LENGTH_SHORT).show()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun writeFile(data: String) {
+        val file = File(Environment.getExternalStorageDirectory(), "test.txt")
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplication7Theme {
-        Greeting("Android")
+        try {
+            FileOutputStream(file).use { it.write(data.toByteArray()) }
+            Toast.makeText(this, "File written to ${file.absolutePath}", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun readFile(): String {
+        val file = File(Environment.getExternalStorageDirectory(), "test.txt")
+
+        return if (file.exists()) {
+            file.readText()
+        } else {
+            "File not found"
+        }
     }
 }
